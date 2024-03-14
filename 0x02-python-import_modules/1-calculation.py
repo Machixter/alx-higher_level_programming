ffb964d634fcf194e8ff135c0eb4f351d5fb6ce7
Machixter <tuqamachi7@gmail.com>
2024-03-14 22:53:22 +0300

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,22 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    """ calculate the results """
+    addition = add(a, b)
+    subtraction = sub(a, b)
+    multiplication = mul(a, b)
+    division = div(a, b)
+
+    """ construct the output string """
+    output = f"{a} + {b} = {addition}\n"
+    output += f"{a} - {b} = {subtraction}\n"
+    output += f"{a} * {b} = {multiplication}\n"
+    output += f"{a} / {b} = {division}"
+
+    """ print the output """
+    print(output)
+
+       #print("{} + {} = {}".format(a, b, add(a, b)))
+       #print("{} - {} = {}".format(a, b, sub(a, b)))
+       #print("{} * {} = {}".format(a, b, mul(a, b)))
+       #print("{} / {} = {}".format(a, b, div(a, b)))
